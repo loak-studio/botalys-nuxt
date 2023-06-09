bf85066f1b4a2b3cdd57b01612cf2bec9d2eab3d
Benjamin Geets <bge@tuta.io>
2023-06-09 15:04:35 +0200

    feat: tag

---
+++ components/global/Tag.vue
@@ -0,0 +1,21 @@
+<template>
+  <span class="tag text">
+    <slot/>
+  </span>
+</template>
+
+<style>
+.tag{
+  color: var(--nandor);
+  display: block;
+}
+.tag::after{
+  height: 2px;
+  background: var(--nandor);
+  width: 224px;
+  margin-bottom: 12px;
+  margin-top: 12px;
+  content:'';
+  display: block;
+}
+</style>
