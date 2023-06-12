db3f1f2b71e2186d6c9b2efb1e392a39d9126706
Benjamin <bge@tuta.io>
2023-06-12 16:37:58 +0200

    feat: development steps

---
+++ components/global/DevelopmentSteps.vue
@@ -0,0 +1,208 @@
+<script setup>
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+let tl
+
+const steps = [
+    {
+        label: "Technical specifications",
+        text: "Determination of the product’s desired technical specifications and characteristics and project feasibility study.",
+    },
+    {
+        label: "Non-technical validation",
+        text: "Bilateral validation of project compliance with legislative, pricing, operational and marketing requirements.",
+    },
+    {
+        label: "Rare Plant Sourcing",
+        text: "Establishment of a list of plant and / or seed suppliers who comply with the legislation and both partners requirements.",
+    },
+    {
+        label: "Vitroplants production <br/> & Cultivar selection",
+        text: "Production of plant or seed-derived vitroplants and selection of the most suitable cultivars for BOTALYS’ process and targeted specifications.",
+    },
+    {
+        label: "Process Optimization",
+        text: "Optimization of the cultivation / growth parameters of the selected cultivar to achieve the biomass and composition specs.",
+    },
+    {
+        label: "Industrial Scale-up",
+        text: "Process scale-up from lab to farm, definition of quality assurance procedures and final product validation before production.",
+    },
+];
+
+onMounted(() => {
+    gsap.registerPlugin(ScrollTrigger)
+    gsap.matchMedia().add('(min-width:809px)', () => {
+        tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".development-steps",
+                start: "10% 50%",
+                end: "90% 50%",
+                scrub: true,
+            }
+        })
+        steps.forEach((step, index) => {
+            tl.from(`.development-steps__item:nth-child(${index + 2}) .development-steps__line-2`, {
+                scale: 0,
+            })
+            tl.from(`.development-steps__item:nth-child(${index + 2}) .development-steps__line-1`, {
+                scale: 0,
+            })
+            tl.from(`.development-steps__item:nth-child(${index + 2})`, {
+                borderWidth: 0,
+                background:'transparent'
+            })
+            tl.from(`.development-steps__item:nth-child(${index + 2}) > *:not(.development-steps__line-1):not(.development-steps__line-2)`, {
+                opacity: 0,
+            })
+        })
+    })
+})
+onUnmounted(() => {
+    tl.kill()
+})
+
+</script>
+<template>
+    <div class="development-steps__parent">
+        <section class="development-steps">
+            <div class="development-steps__container">
+                <div class="development-steps__item-container">
+                    <div class="development-steps__item" v-for="(step, index) in steps" :key="index">
+                        <span class="development-steps__number">{{ index + 1 }}</span>
+                        <h3 class="h3 development-steps__title" v-html="step.label"></h3>
+                        <p class="text" v-html="step.text"></p>
+                        <hr class="development-steps__line-1">
+                        <hr class="development-steps__line-2">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<style lang="scss">
+.development-steps__parent {
+    padding: 0 4px;
+}
+
+.development-steps {
+    max-width: 1296px;
+    margin: 0 auto;
+    padding: 120px 4px  120px 4px;
+    border-left: 2px solid var(--nandor);
+    border-right: 2px solid var(--nandor);
+}
+
+.development-steps__container {
+    max-width: 924px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+}
+
+.development-steps__item {
+    padding: 20px;
+    max-width: 524px;
+    border: 2px solid var(--nandor);
+    position: relative;
+
+    .development-steps__line-1 {
+        height: 87px;
+        width: 2px;
+        background: var(--nandor);
+        content: '';
+        display: block;
+        position: absolute;
+        bottom: 100%;
+        transform-origin: top;
+    }
+
+    .development-steps__line-2 {
+        width: 166px;
+        height: 2px;
+        background-color: var(--nandor);
+        position: absolute;
+        bottom: calc(100% + 87px);
+        content: '';
+        z-index: -1;
+    }
+}
+
+.development-steps__item:nth-child(1) {
+
+    .development-steps__line-1,
+    .development-steps__line-2 {
+        display: none;
+    }
+}
+
+//WHITE BACKGROUND
+.development-steps__item:nth-child(even) {
+    margin: 39px 0 39px auto;
+    background-color: var(--half-spanish-white);
+
+    .development-steps__number {
+        color: var(--half-spanish-white);
+        -webkit-text-stroke: 1px var(--dark-jungle-green);
+    }
+
+    .development-steps__line-1 {
+        left: 55%;
+    }
+
+    .development-steps__line-2 {
+        left: calc(55% - 164px);
+        transform-origin: left;
+    }
+}
+
+
+//BLACK BACKGROUND
+.development-steps__item:nth-child(odd) {
+    background: var(--dark-jungle-green);
+    color: var(--half-spanish-white);
+
+    .development-steps__line-1 {
+        left: 45%;
+    }
+
+    .development-steps__line-2 {
+        left: 45%;
+        transform-origin: right;
+    }
+}
+
+.development-steps__number {
+    font-family: 'Sofia Pro';
+    font-size: 96px;
+    font-weight: 700;
+    line-height: 142%;
+}
+
+.development-steps__title::after {
+    content: '';
+    display: block;
+    height: 2px;
+    width: 100px;
+    background: var(--mikado-yellow);
+    margin: 1rem 0;
+}
+
+@media (max-width: 809px) {
+    .development-steps {
+        padding: 0;
+    }
+
+    .development-steps__container .development-steps__item {
+        margin: 0;
+        max-width: none;
+        border: none;
+        padding: 1rem;
+
+        .development-steps__line-1,
+        .development-steps__line-2 {
+            display: none;
+        }
+    }
+}</style>
--- pages/botanicals/other.vue
+++ pages/botanicals/other.vue
@@ -1,11 +1,30 @@
 <script setup>
 import Plant from "~/assets/images/illustrations/plant.png"
+import River from "~/assets/images/river.jpg"
 </script>
 <template>
     <hero-with-illustration :links="[{
-        href:'#',
-        label:'Other botanicals'
+        href: '#',
+        label: 'Other botanicals'
     },]" :image="Plant" title="Other botanicals" :paragraphs="[
-        'The R&D department at BOTALYS is currently working on the development of several other rare medicinal plants for commercialization.'
-    ]"/>
+    'The R&D department at BOTALYS is currently working on the development of several other rare medicinal plants for commercialization.'
+]" />
+    <black-card />
+    <development-steps/>
+    <text-image :image-src="River">
+        <div>
+            <Tag color="yellow">Get in touch</Tag>
+            <h3 class="h3">Let's go further together</h3>
+        </div>
+        <p class="text">Do you have a project in mind for which you wish to unlock the full potential of rare medicinal
+            plants
+            ? Are you interested in exploring the innovation opportunities offered by Biomimetic Indoor Farming? Or maybe
+            you
+            want to learn more about how phytonutrients and emerging trends can synergize into unique new Nutraceutical
+            formulas? Whatever your needs are, we would love to hear from you and help you achieve your vision. Take the
+            first
+            step and reach out to us.</p>
+        <Button href="#" color="outline">Contact us</Button>
+</text-image>
 </template>
+
