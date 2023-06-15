b1db9d85610edba3bbe2588407ef55665b283a7f
Benjamin <bge@tuta.io>
2023-06-15 11:11:41 +0200

    feat(contact): add netlify forms code

--- components/contact/ContactForm.vue
+++ components/contact/ContactForm.vue
@@ -1,102 +1,127 @@
 <script setup>
 const formToDisplay = ref('contact')
-const setFormToDisplay = (value)=>{
+const setFormToDisplay = (value) => {
     formToDisplay.value = value
 }
-const submitForm = (event)=>{
-    console.log(event)
+const submitForm = (event) => {
+    event.preventDefault();
+
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+
+    fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString(),
+    })
+        .then(() => console.log("Form successfully submitted"))
+        .catch((error) => alert(error));
 }
 </script>
 <template>
     <div class="contact-form">
         <div class="contact-form__header">
-            <button :class="{'contact-form__button--active':formToDisplay == 'contact'}" class="contact-form__button h5" @click="setFormToDisplay('contact')">Contact us</button>
-            <button :class="{'contact-form__button--active':formToDisplay == 'job'}"  class="contact-form__button h5" @click="setFormToDisplay('job')">Join our team</button>
+            <button :class="{ 'contact-form__button--active': formToDisplay == 'contact' }" class="contact-form__button h5"
+                @click="setFormToDisplay('contact')">Contact us</button>
+            <button :class="{ 'contact-form__button--active': formToDisplay == 'job' }" class="contact-form__button h5"
+                @click="setFormToDisplay('job')">Join our team</button>
         </div>
-        <form name="contact" @submit.prevent="submitForm" class="contact-form__form" :class="{'contact-form__form--displayed': formToDisplay == 'contact'}" action="" netlify>
-                <contact-input name="email" type="email" label="Email" required/>
-                <contact-input name="company" label="Company" required/>
-                <contact-input name="job-title" label="Job title" required/>
-                <contact-input name="subject" label="Subject" required/>
-                <contact-textarea name="message" label="Message" required/>
+        <form name="contact" @submit.prevent="submitForm" class="contact-form__form"
+            :class="{ 'contact-form__form--displayed': formToDisplay == 'contact' }" action="" netlify>
+            <contact-input name="email" type="email" label="Email" required />
+            <contact-input name="company" label="Company" required />
+            <contact-input name="job-title" label="Job title" required />
+            <contact-input name="subject" label="Subject" required />
+            <contact-textarea name="message" label="Message" required />
 
-                <botalys-button>Send message</botalys-button>
+            <botalys-button>Send message</botalys-button>
         </form>
 
-        <form name="jobs" @submit.prevent="submitForm"  class="contact-form__form" :class="{'contact-form__form--displayed': formToDisplay == 'job'}" action="" netlify>
-                <contact-input name="email" type="email" label="Email" required/>
-                <contact-input name="subject" label="Subject" required/>
-                <contact-textarea name="message" label="Message" required/>
-                <contact-fileinput name="documents" label="Documents"/>
-                <botalys-button>Send message</botalys-button>
+        <form name="jobs" @submit.prevent="submitForm" class="contact-form__form"
+            :class="{ 'contact-form__form--displayed': formToDisplay == 'job' }" action="" netlify>
+            <contact-input name="email" type="email" label="Email" required />
+            <contact-input name="subject" label="Subject" required />
+            <contact-textarea name="message" label="Message" required />
+            <contact-fileinput name="documents" label="Documents" />
+            <botalys-button>Send message</botalys-button>
         </form>
     </div>
 </template>
 
 <style lang="scss">
-.contact-form{
+.contact-form {
     background: var(--nandor);
 }
-.contact-form__form{
+
+.contact-form__form {
     display: none;
-    &--displayed{
+
+    &--displayed {
         display: flex;
         flex-direction: column;
-        gap:12px;
+        gap: 12px;
         align-items: flex-start;
     }
 }
 
-.contact-form__header{
+.contact-form__header {
     display: flex;
-    *{
+
+    * {
         flex: 1 1 0;
         display: flex;
         justify-content: center;
         align-items: center;
         height: 100%;
-        padding: 20px 0 ;
+        padding: 20px 0;
     }
+
     border: 2px solid var(--nandor);
     background: var(--half-spanish-white);
 }
 
-.contact-form__button{
+.contact-form__button {
     color: var(--half-spanish-white);
     cursor: pointer;
-        background: var(--half-spanish-white);
-        color: var(--nandor);
-    &--active{
+    background: var(--half-spanish-white);
+    color: var(--nandor);
+
+    &--active {
         background: var(--nandor);
         color: var(--half-spanish-white);
     }
 }
-.contact-form__form{
+
+.contact-form__form {
     padding: 2rem 3rem;
 }
-.contact-form__button:nth-child(1){
+
+.contact-form__button:nth-child(1) {
     position: relative;
     z-index: 2;
     border-radius: 0 10px 0px 0;
 }
-.contact-form__button--active:nth-child(1){
+
+.contact-form__button--active:nth-child(1) {
     border-radius: 0 10px 0 0;
 }
-.contact-form__button--active:nth-child(2){
+
+.contact-form__button--active:nth-child(2) {
     position: relative;
-    &::before{
-    height: 100%;
-    width: 30px;
-    content:'';
-    position: absolute;
-    right: 100%;
-    background: var(--nandor);
-    z-index: 0;
-}
-}
-@media (max-width:809px) {
-    .contact-form__form{
-        padding: 1rem;
+
+    &::before {
+        height: 100%;
+        width: 30px;
+        content: '';
+        position: absolute;
+        right: 100%;
+        background: var(--nandor);
+        z-index: 0;
     }
 }
-</style>
+
+@media (max-width:809px) {
+    .contact-form__form {
+        padding: 1rem;
+    }
+}</style>
