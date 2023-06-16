a988c5e45bc27433ab890dd3a040b8bfa11e0c78
Benjamin <bge@tuta.io>
2023-06-16 11:00:37 +0200

    feat: charts test

---
+++ pages/test-charts.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <canvas id="chart" />
+    </div>
+</template>
+
+<style>
+#chart {
+    max-width: 500px;
+    max-height: 400px;
+}
+</style>
+
+<script setup>
+import Chart from 'chart.js/auto';
+
+onMounted(() => {
+    const ctx = document.getElementById('chart');
+    const data = {
+        labels: ['WHITE GINSENG', 'RED GINSENG', 'BOTALYS'],
+        datasets: [{
+            label: 'Rare ginsenosides',
+            data: [2, 4, 40],
+            backgroundColor: '#FFC300',
+            borderColor: "#4D594C",
+            borderWidth: 2
+        }, {
+            label: 'Total Ginsenosides',
+            data: [10, 11, 45],
+            backgroundColor: '#4D594C',
+            borderColor: "#4D594C",
+            borderWidth: 0
+        }]
+    };
+    const config = {
+        type: 'bar',
+        data: data,
+        options: {
+            plugins:{
+                legend:{
+                    position:'bottom',
+                    labels:{
+                        font:{
+                            family:'sofia-pro',
+                        }
+                    }
+                },
+            },
+            scales: {
+                x:{
+                    grid:{display:false}
+                },
+                y: {
+                    title:{
+                        display:true,
+                        text:'Ginsenoside concentration',
+                        font:'sofia-pro'
+                    },
+                    beginAtZero: true,
+                    ticks:{
+                        display:false
+                    },
+                    border:{
+                        display:false
+                    }
+                }
+            }
+        },
+    };
+
+    new Chart(ctx, config)
+})
+</script>
