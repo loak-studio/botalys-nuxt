3d49e52a5e3b35b3b99394b02880a265fb87ac03
Benjamin <bge@tuta.io>
2023-06-23 11:50:31 +0200

    feat(mental-stamina): hero

--- components/global/HeroOverflow.vue
+++ components/global/HeroOverflow.vue
@@ -1,37 +1,37 @@
 <script setup>
-const { links, image, title, paragraphs, top , right, height, width } = defineProps({ links: Array, image:String, title:String, paragraphs:Array, top:String, right:String, width:String, height:String })
+const { links, image, title, paragraphs, top, right, height, width, blendModeLuminosity } = defineProps({ links: Array, image: String, title: String, paragraphs: Array, top: String, right: String, width: String, height: String, blendModeLuminosity: Boolean })
 </script>
 <template>
     <div class="hero-overflow__parent">
         <section class="hero-overflow">
             <breadcrumb :links="links" />
             <div class="hero-overflow__content">
-                <figure :style="`
+                <figure :class="{ 'hero-overflow__figure--blend': blendModeLuminosity }" :style="`
                 --right:${right};
-                --top:${top}
-                --height:${height};
-                --width:${width};
-                
+                                --top:${top}
+                                --height:${height};
+                                --width:${width};
                 `" class="hero-overflow__figure">
-                <img :src="image" alt="">
-            </figure>
-            <div class="hero-overflow__text">
-                <h1 class="h1 hero-overflow__title">{{ title }}</h1>
-                <div>
-                    <p class="h4" v-for="(paragraph,index) in paragraphs" :key="index">{{ paragraph }}</p>
+                    <img :src="image" alt="">
+                </figure>
+                <div class="hero-overflow__text">
+                    <h1 class="h1 hero-overflow__title">{{ title }}</h1>
+                    <div>
+                        <p class="h4" v-for="(paragraph, index) in paragraphs" :key="index">{{ paragraph }}</p>
+                    </div>
                 </div>
-            </div>
             </div>
         </section>
     </div>
 </template>
 
 <style lang="scss">
-.hero-overflow__parent{
+.hero-overflow__parent {
     padding: 0 4px;
-    
+
 }
-.hero-overflow{
+
+.hero-overflow {
     max-width: 1296px;
     margin: 0 auto;
     width: 100%;
@@ -41,30 +41,35 @@ const { links, image, title, paragraphs, top , right, height, width } = definePr
     position: relative;
     border-bottom: 2px solid var(--nandor);
 }
-.hero-overflow__content{
+
+.hero-overflow__content {
     display: flex;
     flex-direction: row-reverse;
     justify-content: space-between;
     padding-top: 10px;
-    gap:1rem;
+    gap: 1rem;
     padding-left: 1rem;
-    *{
+
+    * {
         max-width: 616px;
     }
 }
-.hero-overflow__text{
+
+.hero-overflow__text {
     max-width: 1024px;
     margin: 0 auto;
     width: 100%;
 }
-.hero-overflow__title{
+
+.hero-overflow__title {
     height: 329px;
     display: flex;
     align-items: flex-end;
     padding-bottom: 62px;
-    &::after{
+
+    &::after {
         width: 100%;
-        content:'';
+        content: '';
         height: 2px;
         position: absolute;
         background: var(--nandor);
@@ -73,44 +78,57 @@ const { links, image, title, paragraphs, top , right, height, width } = definePr
         z-index: -1;
     }
 }
-.hero-overflow__figure{
+
+.hero-overflow__figure {
     width: var(--width);
     height: var(--height);
     position: absolute;
     right: var(--right);
     top: var(--top);
-    img{
+
+    img {
         height: 100%;
         width: 100%;
         object-fit: contain;
         object-position: center;
     }
+
+    &--blend {
+        img {
+            mix-blend-mode: luminosity;
+        }
+    }
 }
+
 @media (max-width: 1200px) {
-    .hero-overflow__figure{
+    .hero-overflow__figure {
         width: calc(100% - 700px);
         right: 50px;
         height: 100%;
     }
 }
+
 @media (max-width:900px) {
-    .hero-overflow{
+    .hero-overflow {
         min-height: 0;
     }
-    .hero-overflow__content{
+
+    .hero-overflow__content {
         flex-direction: column;
         padding: 1rem;
     }
-    .hero-overflow__figure{
-        width: 95px ;
-        height: 154px ;
+
+    .hero-overflow__figure {
+        width: 95px;
+        height: 154px;
         position: static;
     }
-    .hero-overflow__text{
+
+    .hero-overflow__text {
         padding: 0 0 96px 0;
     }
-    .hero-overflow__title{
+
+    .hero-overflow__title {
         height: auto;
     }
-}
-</style>
+}</style>
--- pages/botanicals/korean-ginseng/ginseng-for-mental-stamina.vue
+++ pages/botanicals/korean-ginseng/ginseng-for-mental-stamina.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Brain from "~/assets/images/illustrations/brain.png"
 const title = "Ginseng for mental stamina - Botalys"
 useSeoMeta({
   title: title,
@@ -9,4 +10,22 @@ useSeoMeta({
   twitterCard: 'summary_large_image',
 })</script>
 
-<template></template>
+<template>
+    <hero-overflow :links="[
+    {
+      label:'Korean Ginseng', href:'#'
+    }
+  ]"
+  :blend-mode-luminosity="true"
+  :image="Brain"
+  title="Korean Ginseng"
+  right="-214px"
+  top="44px"
+  width="597px"
+  height="895px"
+  :paragraphs="[
+    'Korean ginseng (Panax ginseng C.A. Meyer) is the most emblematic herb of traditional Asian Medicine.',
+    'And while its reputation as a tonic, aphrodisiac and elixir of longevity dates back several millennia, the benefits of this ancient panacea remain more relevant than ever in the field of medicinal plants.'
+  ]"
+  />
+</template>
