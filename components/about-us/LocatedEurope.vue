fc94d1d4e780cd61b82d047a00d9cab5146907ee
Benjamin <bge@tuta.io>
2023-07-04 11:11:38 +0200

    feat(about-us): located europe & contact

---
+++ components/about-us/LocatedEurope.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="located-europe__parent">
+        <section class="located-europe">
+            <div class="located-europe__title">
+                <Tag>Our base camp</Tag>
+                <h2 class="h2">
+                    Located at the heart of Europe
+                </h2>
+                <p class="text">Based in Belgium, BOTALYS’ runs one of the world's largest Vertical Farms devoted to medicinal plant cultivation. Fully operated in the EU under the strictest quality standards, BOTALYS' vertically integrated process from seed to ingredient is testimony to an unwavering commitment to excellence.</p>
+            </div>
+            <figure class="located-europe__draw">
+                    <img src="~/assets/images/located-at-the-heart-of-europe.svg" alt="">
+            </figure>
+        </section>
+    </div>
+</template>
+
+<style lang="scss">
+.located-europe__parent{
+    padding: 0 4px;
+}
+.located-europe{
+    border: 2px solid var(--nandor);
+    max-width: 1296px;
+    width: 100%;
+    margin: 0 auto;
+    padding-top: 120px;
+    padding-bottom: 120px;
+}
+.located-europe__title{
+    max-width: 840px;
+    width: 100%;
+    margin: 0 auto;
+    .text{
+        margin-top: 1rem;
+    }
+
+}
+
+.located-europe__draw{
+    max-width: 1136px;
+    width: 100%;
+    margin: 0 auto;
+    margin-top: 80px;
+    img{
+        height: 100%;
+        width: 100%;
+        object-fit: contain;
+    }
+}
+
+@media (max-width:1200px) {
+    .located-europe{
+        padding: 120px 1rem;
+    }
+}
+@media (max-width:809px) {
+    .located-europe{
+        padding: 40px 1rem;
+    }
+    .located-europe__draw{
+        margin-top: 2rem;
+    }
+}
+</style>
--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Mountain from "~/assets/images/korean-ginseng-moutain.jpeg"
+import River from "~/assets/images/river.jpg"
 const title = "About us - Botalys"
 useSeoMeta({
   title: title,
@@ -8,20 +9,31 @@ useSeoMeta({
   ogImage: 'https://botalys.com/share.png',
   twitterImage: 'https://botalys.com/share.png',
   twitterCard: 'summary_large_image',
-})</script>
+})
+</script>
 
 <template>
-  <hero-with-photo :image="Mountain" title="About us"
-  :breadcrumb-links="[
+  <hero-with-photo :image="Mountain" title="About us" :breadcrumb-links="[
     {
-      label:'About Us',
-      href:'#'
+      label: 'About Us',
+      href: '#'
     }
-  ]"
-        :paragraphs="[
-            `BOTALYS is an ambitious Belgian company dedicated to producing ultra-pure premium botanicals for health.`,
-            `Our team of botanical explorers thrives on cultivating collective intelligence and harvesting daring ideas to put them at the service of human health and well-being.`
-            ]" />
-            <about-us-board-of-directors/>
-            <about-us-a-botanical-odyssey/>
-</template>
+  ]" :paragraphs="[
+  `BOTALYS is an ambitious Belgian company dedicated to producing ultra-pure premium botanicals for health.`,
+  `Our team of botanical explorers thrives on cultivating collective intelligence and harvesting daring ideas to put them at the service of human health and well-being.`
+]" />
+  <about-us-board-of-directors />
+  <about-us-a-botanical-odyssey />
+  <about-us-located-europe />
+  <text-image :imageSrc="River">
+    <div>
+      <Tag color="yellow">Get in touch</Tag>
+      <h3 class="h3">Let's go further together</h3>
+    </div>
+    <p class="text">Do you have a project in mind for which you wish to unlock the full potential of rare medicinal plants
+      ? Are you interested in exploring the innovation opportunities offered by Biomimetic Indoor Farming? Or maybe you
+      want to learn more about how phytonutrients and emerging trends can synergize into unique new Nutraceutical
+      formulas? Whatever your needs are, we would love to hear from you and help you achieve your vision. Take the first
+      step and reach out to us.</p>
+    <botalys-button href="/contact-us/" color="outline">Contact us</botalys-button>
+  </text-image></template>
