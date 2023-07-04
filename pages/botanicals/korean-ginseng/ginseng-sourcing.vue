de8ef653a4b0a390b7a2dd73a805a24f12762c97
Benjamin <bge@tuta.io>
2023-07-04 11:48:20 +0200

    feat(ginseng-sourcing): add contact

--- pages/botanicals/korean-ginseng/ginseng-sourcing.vue
+++ pages/botanicals/korean-ginseng/ginseng-sourcing.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Mountain from "~/assets/images/korean-ginseng-moutain.jpeg"
 const title = "Ginseng sourcing - Botalys"
+import River from "~/assets/images/river.jpg"
 useSeoMeta({
   title: title,
   ogTitle: title,
@@ -19,4 +20,16 @@ useSeoMeta({
         :paragraphs="[
             `With a global market estimated at 80,000 tons per year and a medicinal use spanning over 4 millennia, Korean ginseng is one of the most valuable top selling botanicals in the world, and most of all: a medicinal plant whose history and tradition are deeply intertwined with that of humanity.`]" />
       <botanicals-korean-ginseng-ginseng-sourcing-origin-and-species/>
+      <text-image :imageSrc="River">
+    <div>
+      <Tag color="yellow">Get in touch</Tag>
+      <h3 class="h3">Let's go further together</h3>
+    </div>
+    <p class="text">Do you have a project in mind for which you wish to unlock the full potential of rare medicinal plants
+      ? Are you interested in exploring the innovation opportunities offered by Biomimetic Indoor Farming? Or maybe you
+      want to learn more about how phytonutrients and emerging trends can synergize into unique new Nutraceutical
+      formulas? Whatever your needs are, we would love to hear from you and help you achieve your vision. Take the first
+      step and reach out to us.</p>
+    <botalys-button href="/contact-us/" color="outline">Contact us</botalys-button>
+  </text-image>
 </template>
