382f54d26764a81714e4631c7f0ddc24c9142226
Benjamin <bge@tuta.io>
2023-07-14 11:55:02 +0200

    fix(hero-overflow): remove nuxt-image to prevent weird black border

--- components/global/HeroOverflow.vue
+++ components/global/HeroOverflow.vue
@@ -12,7 +12,7 @@ const { links, image, title, paragraphs, top, right, height, width, blendModeLum
                                 --height:${height};
                                 --width:${width};
                 `" class="hero-overflow__figure">
-                    <nuxt-picture loading="lazy" :width="width" :height="height" :src="image" alt=""/>
+                    <img :src="image" alt=""/>
                 </figure>
                 <div class="hero-overflow__text">
                     <h1 class="h1 hero-overflow__title">{{ title }}</h1>
@@ -85,6 +85,7 @@ const { links, image, title, paragraphs, top, right, height, width, blendModeLum
     position: absolute;
     right: var(--right);
     top: var(--top);
+
 
     img {
         height: 100%;
